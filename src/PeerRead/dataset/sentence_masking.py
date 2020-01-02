6854eb0a5174630bfccf6cefbfbff5ab8ffb9d82
vveitch <victorveitch@gmail.com>
2020-01-02 14:20:23 -0500

    bug fix where comment string and future import caused a fuckup

--- src/PeerRead/dataset/sentence_masking.py
+++ src/PeerRead/dataset/sentence_masking.py
@@ -1,19 +1,5 @@
-# coding=utf-8
-# Copyright 2018 The Google AI Language Team Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific PeerRead governing permissions and
-# limitations under the License.
-"""Create masked LM TF examples for BERT."""
-"""heavily modified from original tensorflow masking code by victor veitch"""
+# dynamic word piece masking for BERT models
+# approximately adapted from The Google AI Language Team Authors (Apache 2018)
 
 from __future__ import absolute_import
 from __future__ import division
