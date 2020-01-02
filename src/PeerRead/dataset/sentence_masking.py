bug fix to sentence masking. very short sentences would sometimes cause an error when exactly 1 token was masked.

src/PeerRead/dataset/sentence_masking.py
@@ -13,6 +13,7 @@
 # See the License for the specific PeerRead governing permissions and
 # limitations under the License.
 """Create masked LM TF examples for BERT."""
+"""heavily modified from original tensorflow masking code by victor veitch"""
 
 from __future__ import absolute_import
 from __future__ import division
@@ -21,7 +22,6 @@ from __future__ import print_function
 import collections
 
 import tensorflow as tf
-
 
 MaskedLmInstance = collections.namedtuple("MaskedLmInstance",
                                           ["index", "label"])
@@ -39,23 +39,22 @@ def create_masked_lm_predictions(token_ids, masked_lm_prob, max_predictions_per_
         tf.random.uniform(token_ids.shape, minval=0, maxval=1, dtype=tf.float32, seed=seed),
         masked_lm_prob)
 
-    # don't sample_weight special characters or padding
+    # don't mask special characters or padding
     cand_indexes = tf.logical_and(tf.not_equal(token_ids, vocab["[CLS]"]),
                                   tf.not_equal(token_ids, vocab["[SEP]"]))
     cand_indexes = tf.logical_and(cand_indexes, tf.not_equal(token_ids, 0))
     mask = tf.logical_and(cand_indexes, basic_mask)
 
-    # truncate to max predictions for ease of padding
-    masked_lm_positions = tf.compat.v1.where(mask)
-    # TODO: it should be essentially impossible for me to see this bug (very unlikely), but I do... symptom of :( ?
-    # very rare event: nothing gets picked for sample_weight, causing an irritating bug
-    # in this case, just sample_weight the first candidate index
-    mlm_shape = tf.shape(input=masked_lm_positions)[0]
-    masked_lm_positions = tf.cond(pred=mlm_shape > 1,
-                                  true_fn=lambda: masked_lm_positions,
-                                  false_fn=lambda: tf.compat.v1.where(cand_indexes)[0:2])
+    # sometimes nothing gets masked. In that case, just mask the first valid token
+    masked_lm_positions = tf.cond(pred=tf.reduce_any(mask),
+                                  true_fn=lambda: tf.where(mask),
+                                  false_fn=lambda: tf.where(cand_indexes)[0:2])
 
-    masked_lm_positions = tf.squeeze(masked_lm_positions)[0:max_predictions_per_seq]
+    masked_lm_positions = masked_lm_positions[:, 0]
+
+    # truncate to max predictions for ease of padding
+    masked_lm_positions = tf.random.shuffle(masked_lm_positions, seed=seed)
+    masked_lm_positions = masked_lm_positions[0:max_predictions_per_seq]
     masked_lm_positions = tf.cast(masked_lm_positions, dtype=tf.int32)
     masked_lm_ids = tf.gather(token_ids, masked_lm_positions)
 
@@ -63,10 +62,10 @@ def create_masked_lm_predictions(token_ids, masked_lm_prob, max_predictions_per_
         tf.scatter_nd(tf.expand_dims(masked_lm_positions, 1), tf.ones_like(masked_lm_positions), token_ids.shape),
         bool)
 
-    output_ids = tf.compat.v1.where(mask, vocab["[MASK]"]*tf.ones_like(token_ids), token_ids)
+    output_ids = tf.where(mask, vocab["[MASK]"] * tf.ones_like(token_ids), token_ids)
 
     # pad out to max_predictions_per_seq
-    masked_lm_weights = tf.ones_like(masked_lm_ids, dtype=tf.float32) # tracks padding
+    masked_lm_weights = tf.ones_like(masked_lm_ids, dtype=tf.float32)  # tracks padding
     add_pad = [[0, max_predictions_per_seq - tf.shape(input=masked_lm_positions)[0]]]
     masked_lm_weights = tf.pad(tensor=masked_lm_weights, paddings=add_pad, mode='constant')
     masked_lm_positions = tf.pad(tensor=masked_lm_positions, paddings=add_pad, mode='constant')