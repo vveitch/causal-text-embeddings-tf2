7fa4a6bfd8fe8c071aca4f111c3a96507f25a705
victor <victorveitch@gmail.com>
2020-01-03 20:14:58 -0500

    collecting dataframe utils, and adding documentation

---
+++ src/causal_bert/data_utils.py
@@ -0,0 +1,73 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+
+# dynamic word piece masking for BERT models
+# sort-of adapted from The Google AI Language Team Authors (Apache 2018)
+def create_masked_lm_predictions(token_ids: tf.Tensor,
+                                 masked_lm_prob: float,
+                                 max_predictions_per_seq: int,
+                                 vocab: dict,
+                                 seed: int):
+    """
+    Randomly masks tokens to produce (one of) the BERT unsupervised learning objectives
+
+    Args:
+        token_ids: the ids to be masked
+        masked_lm_prob: per-token masking probability
+        max_predictions_per_seq: maximum allowed number of masks
+        vocab: the vocabulary used for tokenization
+        seed: random seed
+
+    Returns: output_ids, masked_lm_positions, masked_lm_ids, masked_lm_weights as expected by BERT model
+
+    """
+
+    basic_mask = tf.less(
+        tf.random.uniform(token_ids.shape, minval=0, maxval=1, dtype=tf.float32, seed=seed),
+        masked_lm_prob)
+
+    # don't mask special characters or padding
+    cand_indexes = tf.logical_and(tf.not_equal(token_ids, vocab["[CLS]"]),
+                                  tf.not_equal(token_ids, vocab["[SEP]"]))
+    cand_indexes = tf.logical_and(cand_indexes, tf.not_equal(token_ids, 0))
+    mask = tf.logical_and(cand_indexes, basic_mask)
+
+    # sometimes nothing gets masked. In that case, just mask the first valid token
+    masked_lm_positions = tf.cond(pred=tf.reduce_any(mask),
+                                  true_fn=lambda: tf.where(mask),
+                                  false_fn=lambda: tf.where(cand_indexes)[0:2])
+
+    masked_lm_positions = masked_lm_positions[:, 0]
+
+    # truncate to max predictions for ease of padding
+    masked_lm_positions = tf.random.shuffle(masked_lm_positions, seed=seed)
+    masked_lm_positions = masked_lm_positions[0:max_predictions_per_seq]
+    masked_lm_positions = tf.cast(masked_lm_positions, dtype=tf.int32)
+    masked_lm_ids = tf.gather(token_ids, masked_lm_positions)
+
+    mask = tf.cast(
+        tf.scatter_nd(tf.expand_dims(masked_lm_positions, 1), tf.ones_like(masked_lm_positions), token_ids.shape),
+        bool)
+
+    output_ids = tf.where(mask, vocab["[MASK]"] * tf.ones_like(token_ids), token_ids)
+
+    # pad out to max_predictions_per_seq
+    masked_lm_weights = tf.ones_like(masked_lm_ids, dtype=tf.float32)  # tracks padding
+    add_pad = [[0, max_predictions_per_seq - tf.shape(input=masked_lm_positions)[0]]]
+    masked_lm_weights = tf.pad(tensor=masked_lm_weights, paddings=add_pad, mode='constant')
+    masked_lm_positions = tf.pad(tensor=masked_lm_positions, paddings=add_pad, mode='constant')
+    masked_lm_ids = tf.pad(tensor=masked_lm_ids, paddings=add_pad, mode='constant')
+
+    return output_ids, masked_lm_positions, masked_lm_ids, masked_lm_weights
+
+
+def main(_):
+    pass
+
+
+if __name__ == "__main__":
+    main()
