c6b3dd19fdcc329e869b2228838f8480893e55ca
victor <victorveitch@gmail.com>
2020-01-07 14:22:42 -0500

    tracking down increasing loss bug

--- src/PeerRead/submit_scripts/emayhem/run_causal_bert.sh
+++ src/PeerRead/submit_scripts/emayhem/run_causal_bert.sh
@@ -13,7 +13,7 @@ export INIT_FILE=$INIT_DIR/bert_model.ckpt-102
 export BERT_BASE_DIR=/proj/sml_netapp/projects/victor/causal-text-tf2/pre-trained/uncased_L-12_H-768_A-12
 #export INIT_FILE=$BERT_BASE_DIR/bert_model.ckpt
 export DATA_FILE=/proj/sml_netapp/dat/undocumented/PeerRead/proc/arxiv-all.tf_record
-export OUTPUT_DIR=/proj/sml_netapp/projects/victor/causal-text-tf2/out/cb_test
+export OUTPUT_DIR=/proj/sml_netapp/projects/victor/causal-text-tf2/out/cb_test-halfdatasize
 export PREDICTION_FILE=$OUTPUT_DIR/predictions.tsv
 
 python -m PeerRead.model.run_causal_bert \
