0472d06372612c4673a03d4900907249dca44dec
Dhanya Sridhar <dhanyasridhar@Dhanyas-MacBook-Pro.local>
2020-01-28 17:12:08 -0500

    adding options to train only to debug

--- src/reddit/model/run_causal_bert.py
+++ src/reddit/model/run_causal_bert.py
@@ -38,8 +38,9 @@ from reddit.dataset.dataset import make_input_fn_from_file, make_real_labeler, m
 
 common_flags.define_common_bert_flags()
 
+#REMEMBER TO CHANGE THIS BACK
 flags.DEFINE_enum(
-    'mode', 'train_and_predict', ['train_only', 'train_and_predict', 'predict_only'],
+    'mode', 'train_only', ['train_only', 'train_and_predict', 'predict_only'],
     'One of {"train_and_predict", "predict_only"}. `train_and_predict`: '
     'trains the model and make predictions. '
     '`predict_only`: loads a trained model and makes predictions.')
