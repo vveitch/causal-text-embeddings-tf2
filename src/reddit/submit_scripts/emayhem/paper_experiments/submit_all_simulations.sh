1b93d09795a6d4609aa3d9e3989de35271c2712f
victor <victorveitch@gmail.com>
2020-02-03 15:31:04 -0500

    filename typos

--- src/reddit/submit_scripts/emayhem/paper_experiments/submit_all_simulations.sh
+++ src/reddit/submit_scripts/emayhem/paper_experiments/submit_all_simulations.sh
@@ -2,7 +2,7 @@
 
 # Jan 13, 2020
 ./reddit/submit_scripts/emayhem/paper_experiments/submit_subreddit_sim.sh
-./reddit/submit_scripts/emayhem/paper_experiments/submit_subreddit_fixed_feature_baseline.sh
-./reddit/submit_scripts/emayhem/paper_experiments/submit_subreddit_no_dragon_baseline.sh
-./reddit/submit_scripts/emayhem/paper_experiments/submit_subreddit_no_init_baseline.sh
+./reddit/submit_scripts/emayhem/paper_experiments/run_subreddit_fixed_features.sh
+./reddit/submit_scripts/emayhem/paper_experiments/run_subreddit_no_dragon_baseline.sh
+.reddit/submit_scripts/emayhem/paper_experiments/submit_subreddit_no_init_baseline.sh
 ./reddit/submit_scripts/emayhem/paper_experiments/submit_subreddit_no_masking_baseline.sh
